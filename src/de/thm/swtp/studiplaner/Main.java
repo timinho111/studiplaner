25bb0032f77354d04e75f0df508a7a46f05d46fc
AI2211 <tolik1990@hotmail.de>
2017-10-22 22:58:29 +0200

    TEST

--- src/de/thm/swtp/studiplaner/Main.java
+++ src/de/thm/swtp/studiplaner/Main.java
@@ -1,6 +1,7 @@
 package de.thm.swtp.studiplaner;
 	
 import javafx.application.Application;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -11,8 +12,8 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("GUI.fxml"));
-			Scene scene = new Scene(root,400,400);
+			AnchorPane root = FXMLLoader.load(getClass().getResource("view/Login.fxml"));
+			Scene scene = new Scene(root,432,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
