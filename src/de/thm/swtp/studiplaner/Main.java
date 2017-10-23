59f1f437ece1c09ee458d330eca3791f5cca6f19
Unknown <thefreakintim@gmail.com>
2017-10-23 15:32:22 +0200

    Realization of Dashboard - Standard GUI implemented
    WIP

--- src/de/thm/swtp/studiplaner/Main.java
+++ src/de/thm/swtp/studiplaner/Main.java
@@ -11,9 +11,11 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("GUI.fxml"));
-			Scene scene = new Scene(root,400,400);
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("view/Dashboard.fxml"));
+			Scene scene = new Scene(root,1366,768);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setMaximized(true);
+			primaryStage.setFullScreen(true);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
