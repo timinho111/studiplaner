2b195006f2f18b5724cf950e6a77e02ca85d59cd
Unknown <thefreakintim@gmail.com>
2017-12-15 16:17:58 +0100

    Base of Calendar added
    Added Calendar via DatePickerSkin

--- src/de/thm/swtp/studiplaner/Main.java
+++ src/de/thm/swtp/studiplaner/Main.java
@@ -14,8 +14,6 @@ public class Main extends Application {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("view/fxml/Main.fxml"));
 			Scene scene = new Scene(root,1366,768);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setMaximized(true);
-			primaryStage.setFullScreen(true);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
